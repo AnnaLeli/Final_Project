65e00dc7705e44cd430f2ff715e073b901426d39
LA <annleli@yandex.ru>
2022-10-26 22:41:34 +0300

    task

---
+++ task/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string[] array = new string[8]{"1234", "1567", "-2", "computer science", "Hello", "2", "world", ":-)"};
+void Result(string[] array)
+{
+for (int index = 0; index < array.Length; index++)
+    {
+    if(array[index].Length <= 3)
+        {
+    //    arrayShort[index] = array[count];
+        Console.Write($"{array[index]} ");
+    //    count++;
+        }
+    }
+}
+Result(array);
