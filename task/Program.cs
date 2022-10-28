0be31e6366899e5f7accd4f8207316d190d86e7a
LA <annleli@yandex.ru>
2022-10-28 20:21:03 +0300

    task redacted

--- task/Program.cs
+++ task/Program.cs
@@ -1,13 +1,25 @@
 ﻿
 string[] array = new string[8]{"1234", "1567", "-2", "computer science", "Hello", "2", "world", ":-)"};
-void Result(string[] array)
-{
+Console.Write("Начальный массив: ");
+PrintArray(array);
+string[] arrayNew = new string[array.Length];
+Console.WriteLine();
+int count = 0;
 for (int index = 0; index < array.Length; index++)
     {
     if(array[index].Length <= 3)
         {
-        Console.Write($"{array[index]} ");
+        arrayNew[count] = array[index];
+        count++;
         }
     }
+void PrintArray(string[] arrayNew)
+{
+    for (int index = 0; index < arrayNew.Length; index++)
+    {
+        Console.Write($"{arrayNew[index]} ");
+    }
 }
-Result(array);
+
+Console.Write("Итоговый массив: ");
+PrintArray(arrayNew);
